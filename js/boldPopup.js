c885ccea0594dd236d47abac370334dc8b2aea35
Krystian Wolarek <wolarekkrystian@gmail.com>
2016-03-30 20:51:52 +0200

    dodanie  getera do popup

--- js/boldPopup.js
+++ js/boldPopup.js
@@ -1,37 +1,59 @@
 var boldPopup = (function(){
 	var instance = this,
-		popups ={};
+		extend = boldExtend,
+		popups ={},
 
-	function Popup(options) {
-		this.popupId= options.id;
-
-		this.constructor(options);
-	}
-
-	Popup.prototype.constructor = function(options) {
-	
-	};
-
-	Popup.prototype.show = function() {
-		var _this= this;
-		console.log('showing ' + _this.popupId)
-	};
-
-	Popup.prototype.hide = function() {
-		var _this= this;
-		console.log('hide ' + _this.popupId)
-	};
-
-	var popupFactory = function (options) {
+	popupFactory = function (options) {
 		var newTemplate = new Popup(options);
 
 		popups[options.id] = newTemplate;
 
 		return newTemplate;
+	},
+
+	getPopup = function(name){
+		return popups[name];
 	};
+
+	function Popup(options) {
+		var defaultOptions ={
+			'id': 'defaultPopup'
+		}
+		
+
+		this.constructor(options);
+	}
+
+	Popup.prototype.constructor = function(opt) {
+		var _this = this;
+
+		_this.options = extend(_this.defaultOptions, opt)
+
+		this.init();
+	};
+
+	Popup.prototype.init = function() {
+		var _this = this;
+			
+	}
+	Popup.prototype.getOption = function(optionName) {
+		return this.options[optionName];
+	}
+
+	Popup.prototype.show = function() {
+		var _this= this;
+		console.log('showing ' + _this.getOption('id'))
+	};
+
+	Popup.prototype.hide = function() {
+		var _this= this;
+		console.log('hide ' + _this.getOption('id'))
+	};
+
 	return {
 		create: popupFactory,
-		popups: popups
+		get: getPopup,
+		listPopups: popups
 	};
 
 } ());
